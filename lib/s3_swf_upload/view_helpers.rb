def4d7859ae24133a0bb7e7291afff3104bab4c9
Zhang Yuanyi <yzhang@elctech.com>
2009-02-26 15:14:13 +0800

    add rails helper

--- lib/s3_swf_upload/view_helpers.rb
+++ lib/s3_swf_upload/view_helpers.rb
@@ -1,35 +1,42 @@
 module S3SwfUpload
   module ViewHelpers
     def s3_swf_upload_tag(options = {})
-      height    = options[:height] || 35
-      width     = options[:width]  || 300
-      js_helper = options[:js_helper].nil? ? true : options[:js_helper]
+      height     = options[:height] || 35
+      width      = options[:width]  || 300
+      success    = options[:success]  || ''
+      failed     = options[:failed]  || ''
+      selected   = options[:selected]  || ''
+      canceled     = options[:canceled] || ''
+      prefix     = options[:prefix] || 's3_swf'
       
       out = ""
       out << %(
-        <script type="text/javascript" src="/javascripts/swfobject.js"></script>
+        <script type="text/javascript" src="/javascripts/s3_upload.js"></script>
+
         <script type="text/javascript">
-          swfobject.embedSWF("/s3_upload.swf", "s3_upload", "#{width}", "#{height}", "9.0.0");
+            s3_swf = s3_swf_init('s3_upload', {
+              width:  #{width},
+              height: #{height},
+              onSuccess: function(){
+                #{success}
+              },
+              onFailed: function(status){
+                #{failed}
+              },
+              onFileSelected: function(filename, size){
+                #{selected}
+              },
+              onCancel: function(){
+                #{canceled}
+              }
+            });
         </script>
-      )
-      
-      out << %(
+
         <div id="s3_upload">
           Please <a href="http://www.adobe.com/go/getflashplayer">Update</a> your Flash Player to Flash v9.0.1 or higher...
         </div>
-      )
-      
-      out << s3_swf_upload_init if js_helper
-    end
-    
-    def s3_swf_upload_init(options={})
-      signature_url = options[:signature_url] || s3_signatures_url
-      %(
-        <script language="JavaScript" type="text/javascript">
-        <!--
-          function s3_upload() { document["s3_upload"].init("#{signature_url}"); }
-        // -->
-        </script>
+
+        <a href="#" onclick="s3_swf.upload('#{prefix}/')">Upload</a>
       )
     end
   end
